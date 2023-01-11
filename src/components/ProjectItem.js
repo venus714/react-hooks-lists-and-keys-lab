7d1fb54b7e3cedf513628764c3a70f5df3c27a00
Student <venus.kabera@student.moringaschool.com>
2023-01-11 16:00:45 +0300

    done with assignment

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,15 @@
 import React from "react";
 
 function ProjectItem({ name, about, technologies }) {
+  const technol=technologies.map((techn)=>(
+    <span key={techn}>{techn}</span>
+  ))
   return (
     <div className="project-item">
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {/* render a <span> for each technology in the technologies array */technol}
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import NavBar from "./components/NavBar";
+ReactDOM.render(<App  />,<NavBar/> , document.getElementById("root"));
